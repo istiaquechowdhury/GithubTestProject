6509fed37a657a4494e74a6444268ff712a3bd3c
Istiaque <istiaquechowdhury430@gmail.com>
2025-07-16 18:56:00 +0600

    added values controller

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GithubTestProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        public IActionResult Create()
+        {
+            return Ok();
+        }
+    }
+}
